8daa4c404a61ff8bc20c26d1edc890c2b07620af
giovani <giovanimouradev@gmail.com>
2020-12-05 19:08:30 -0300

    Iniciado o projeto

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>@yield('title')</title>
+    
+
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  @extends('layouts.template')
+  @section('title', 'TELA DE LOGIN')
+
+  @section('content')
+  TELA DE LOGIN
+      
+  @endsection
